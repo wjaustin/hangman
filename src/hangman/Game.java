8bd687e4de7f923994e3cd826f0789aaab33638a
Wesley Austin <wjaustin@oakland.edu>
2016-01-11 20:16:08 -0500

    First Commit
    Initial files, bare bones java project

---
+++ src/hangman/Game.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hangman;
+
+/**
+ * @author wjaau_000
+ *
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
