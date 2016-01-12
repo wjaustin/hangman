59b61fc70030f98168a3216e18dbb2a669de1fc6
Wesley Austin <wjaustin@oakland.edu>
2016-01-12 15:08:23 -0500

    Added skeleton classes with java docs for future functionality.

--- src/hangman/Game.java
+++ src/hangman/Game.java
@@ -8,13 +8,44 @@ package hangman;
  *
  */
 public class Game {
+	//TODO initialize variables
 
 	/**
-	 * @param args
+	 * Constructor.  Initializes Hangman game.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Game(){
+		//TODO
 	}
+	/**
+	 * Main loop that will run the game of Hangman.
+	 * The loop will end and the game put into an end state
+	 * when the user wins or presses the Stop Game button.
+	 */
+	public void runHangman(){
+		
+	}
+	
+	/**
+	 * Tests the cases in which a player will have won the game,
+	 * and returns true if the player has won.
+	 * @return true if the player has won, false otherwise.
+	 */
+	private boolean hasWon(){
+		
+		return false;
+	}
+	
+	/**
+	 * Uses a random number picker to select a random word from
+	 * the provided string of words.
+	 * @param words an array containing all the selectable words
+	 * @return a string from words selected at random.
+	 */
+	private String pickWord(String[] words){
+		
+		return null;
+	}
+	
+	
 
 }
